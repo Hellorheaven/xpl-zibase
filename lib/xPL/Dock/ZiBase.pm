9a66e4d9c5d706b45203500fe2e188ca46c32b35
Hellorheaven <mickyradio@hotmail.com>
2012-11-02 22:53:22 +0100

    first publish release 0.21

---
+++ lib/xPL/Dock/ZiBase.pm
@@ -0,0 +1,615 @@
+package xPL::Dock::ZiBase;
+
+=head1 NAME
+
+xPL::Dock::ZiBase - xPL::Dock plugin for Zodianet's ZiBase Home Automation
+controller.
+
+=head1 SYNOPSIS
+
+ use xPL::Dock qw/ZiBase/;
+ my $xpl = xPL::Dock->new(name => 'zibase');
+
+ 
+ $XPL->main_loop();
+
+=head1 DESCRIPTION
+
+This module creates an xPL client for the ZiBase Home Automation controller.
+
+=head1 METHODS
+
+=cut
+
+use Socket;
+use xPL::ZiBase::ZiMessage;
+use strict;
+use warnings;
+
+use English qw/-no_match_vars/;
+use xPL::Dock::Plug;
+
+our @ISA = qw(xPL::Dock::Plug);
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = "0.21";
+
+__PACKAGE__->make_readonly_accessor($_) foreach (qw/rfcom device/);
+
+
+my $vendor_id = 'tlam';
+
+
+=head2 C<getopts( )>
+
+This method returns the L<Getopt::Long> option definition for the
+plugin.
+
+=cut
+
+sub getopts {
+  my $self = shift;
+  return
+    (
+     'zibase-verbose+' => \$self->{_verbose},
+    );
+}
+
+=head2 C<init(%params)>
+
+=cut
+
+sub init {
+  my $self = shift;
+  my $xpl = shift;
+
+  
+  my %p = @_;
+
+  # Ugly force vendor ID
+  $xpl->{'_vendor_id'} = $vendor_id;
+
+  
+  $self->SUPER::init($xpl, @_);
+
+  $self->{_zibase_ip} = "";
+  $self->{_zibase_id} = "";
+  $self->{_listen_port} = 0;
+
+  # Create listening UDP socket
+  my $listen;
+  socket($listen, PF_INET, SOCK_DGRAM, getprotobyname('udp'));
+  binmode $listen;
+
+  # Setup the UDP listen socket
+  my $ip = "0.0.0.0";
+  my $port = 28734;   # Random value berk...
+  my $okay = 0;
+
+  while ($okay == 0) {
+    if (bind($listen, sockaddr_in($port, inet_aton($ip)))) {
+      $okay = 1;
+      $self->{_listen_port} = $port;
+    } else {
+      warn "Could not bind socket to ".$ip.":".$port."... retrying...";
+      sleep 1;
+      $port += 1;
+    }
+    if ($port >= 65535) {
+      die "Could not setup listening socket";
+    }
+  }
+
+  # Listen socket opened and bound successfully
+  print("Listening ZiBase messages on ".$ip.":".$port."\n") if $self->{_verbose};
+
+  $self->{_listen_sock} = $listen;
+  $xpl->add_input(handle => $listen, callback => sub { $self->zibase_message(@_) });
+
+
+  # Send ZiBase discovery packet
+  my $zmsg = new ZiMessage();
+  $zmsg->{_command} = 8;
+
+  my $sin = sockaddr_in(49999, inet_aton("255.255.255.255"));
+  setsockopt $listen, SOL_SOCKET, SO_BROADCAST, 1;
+  send($listen, $zmsg->getBinaryMessage(), 0, $sin);
+  
+
+  # Setup Rf commands hook callback
+
+  $xpl->add_xpl_callback(id => 'xpl-rfcmd', callback => \&xpl_rfcmd,
+                         arguments => $self,
+                         filter =>
+                         {
+                          message_type => 'xpl-cmnd',
+                          class => 'rfcmd',
+                          class_type => 'basic',
+                         }
+						 );
+  # return $self;
+  
+    # Setup x10 commands hook callback
+	$xpl->add_xpl_callback(id => 'xpl-x10cmd', callback => \&xpl_x10cmd,
+                         arguments => $self,
+                         filter =>
+                         {
+                          message_type => 'xpl-cmnd',
+                          class => 'x10',
+                          class_type => 'basic',
+                         });
+
+  # return $self;
+  
+  # Setup scenario commands hook callback
+  $xpl->add_xpl_callback(id => 'xpl-scencmd', callback => \&xpl_scencmd,
+                         arguments => $self,
+                         filter =>
+                         {
+                          message_type => 'xpl-cmnd',
+                          class => 'scenario',
+                          class_type => 'basic',
+                         });
+
+  return $self;
+  
+}
+
+=head2 C<zibase_send_message(ZiMessage)>
+
+Sends the specified ZiMessage over network to ZiBase.
+
+=cut
+
+sub zibase_send_message {
+  my ($self, $zmsg) = @_;
+
+  # Prepare sockaddr in structure
+  my $sin = sockaddr_in(49999, inet_aton($self->{_zibase_ip}));
+  # Force socket options
+  setsockopt $self->{_listen_sock}, SOL_SOCKET, SO_BROADCAST, 1;
+  # Send ZiMessage payload
+  send($self->{_listen_sock}, $zmsg->getBinaryMessage(), 0, $sin);
+}
+
+=head2 C<zibase_register_host()>
+
+Register the host with the ZiBase to start receive messages.
+
+=cut
+
+sub zibase_register_host {
+  my ($self) = @_;
+
+  # Construct zibase message and send it
+  my $zmsg = new ZiMessage();
+  $zmsg->setRegisterHost($self->{_xpl}->{_ip}, $self->{_listen_port});
+  $self->zibase_send_message($zmsg);
+}
+
+=head2 C<xpl_rfcmd(%xpl_callback_parameters)>
+
+This is the callback that processes incoming xPL messages.  It handles
+the incoming rfcmd.basic schema messages.
+
+=cut
+
+sub xpl_rfcmd {
+  my %p = @_;
+  my $msg = $p{message};
+  my $peeraddr = $p{peeraddr};
+  my $peerport = $p{peerport};
+  my $self = $p{arguments};
+
+  my $repeatcnt = $msg->field('repeat') || 1;
+
+  my $m_device = $msg->field('device');
+  my $m_command = $msg->field('command');
+  my $m_protocol = $msg->field('protocol');
+  my $m_level = $msg->field('level');
+
+  # Send corresponding command to zibase
+  $self->zibase_command($m_device, $m_command, $m_protocol, $m_level, $repeatcnt);
+
+  return 1;
+}
+
+=head2 C<xpl_x10(%xpl_callback_parameters)>
+
+This is the callback that processes incoming xPL messages.  It handles
+the incoming x10.basic schema messages.
+
+=cut
+
+sub xpl_x10cmd {
+  my %p = @_;
+  my $msg = $p{message};
+  my $peeraddr = $p{peeraddr};
+  my $peerport = $p{peerport};
+  my $self = $p{arguments};
+
+  my $repeatcnt = $msg->field('repeat') || 1;
+
+  my $m_device = $msg->field('device');
+  my $m_command = $msg->field('command');
+  my $m_protocol = 'x10';
+  my $m_level = $msg->field('level');
+
+  # Send corresponding command to zibase
+  $self->zibase_x10command($m_device, $m_command, $m_protocol, $m_level, $repeatcnt);
+
+  return 1;
+}
+
+=head2 C<xpl_scencmd(%xpl_callback_parameters)>
+
+This is the callback that processes incoming xPL messages.  It handles
+the incoming scencmd.basic schema messages.
+
+=cut
+
+sub xpl_scencmd {
+  my %p = @_;
+  my $msg = $p{message};
+  my $peeraddr = $p{peeraddr};
+  my $peerport = $p{peerport};
+  my $self = $p{arguments};
+
+  my $m_scenario = $msg->field('scenario');
+
+  # Send corresponding command to zibase
+  $self->zibase_execScenario($m_scenario);
+
+  return 1;
+}
+
+=head2 C<zibase_command($device, $command, $protocol, $dimlevel, $nbrepeat)>
+
+Sends the specified RF command to ZiBase
+
+=cut
+
+sub zibase_command {
+  my ($self, $device, $command, $protocol, $level, $nbrepeat) = @_;
+
+  my $zmsg = new ZiMessage();
+  # Set ZiMessage parameters
+  $zmsg->setRFCommand($command, $protocol, $level, $nbrepeat);
+  $zmsg->setRFAddress($device);
+  # Send it over network
+  $self->zibase_send_message($zmsg);
+  # Send corresponding xPL Trigger
+  $self->xpl_send_rfcmd($device, $command, $protocol, $level);
+}
+
+sub zibase_x10command {
+  my ($self, $device, $command, $protocol, $level, $nbrepeat) = @_;
+
+  my $zmsg = new ZiMessage();
+  # Set ZiMessage parameters
+  $zmsg->setRFCommand($command, $protocol, $level, $nbrepeat);
+  $zmsg->setRFAddress($device);
+  # Send it over network
+  $self->zibase_send_message($zmsg);
+  # Send corresponding xPL Trigger
+  $self->xpl_send_x10($device, $command, $protocol, $level);
+}
+
+sub zibase_execScenario {
+  my ($self, $scenario) = @_;
+
+  my $zmsg = new ZiMessage();
+  # Set ZiMessage parameters
+  $zmsg->setRFexecScenario($scenario);
+  #$zmsg->setRFAddress($device);
+  # Send it over network
+  $self->zibase_send_message($zmsg);
+  # Send corresponding xPL Trigger
+  $self->xpl_send_scenario($scenario);
+}
+
+=head2 C<zibase_message($file_handle)>
+
+This method is called when the agent receives a ZiBase message.
+
+=cut
+
+sub zibase_message {
+  my $self = shift;
+  my $sock = $self->{_listen_sock};
+  my $buf = '';
+  my $addr = recv($sock, $buf, 1500, 0);
+  my ($peerport, $peeraddr) = sockaddr_in($addr);
+  $peeraddr = inet_ntoa($peeraddr);
+
+  # Create message and initialize it from received payload
+  my $zmsg = new ZiMessage();
+  $zmsg->fromPayload($buf);
+
+
+  if ($zmsg->is_ack()) {
+    # This is an ACK
+    if ($self->{_zibase_ip} eq "") {
+      # If first received ACK, register ZiBase IP:PORT
+      $self->{_zibase_ip} = $peeraddr;
+      $self->{_zibase_id} = $zmsg->{_zibase_id};
+      print "Found ZiBase '".$zmsg->{_zibase_id}."' at IP ".$peeraddr."\n" if $self->{_verbose};
+      $self->zibase_register_host();
+    }
+  } elsif ($zmsg->is_rfreceive()) {
+    # Print received RF ZiMessage
+    print("Received : ".$zmsg->{_message}."\n") if $self->{_verbose};
+    $self->zibase_rfreceive_decode($zmsg->{_message});
+  } else {
+    warn "Received an unknown Message from ZiBase ".$zmsg->{_command}." !!!";
+  }  
+
+  return (1);
+}
+
+=head2 C<xpl_send_sensor($devid, $unit, $value)>
+
+This method sends a sensor.basic trigger message over the xPL network.
+  devid = device identifier
+  unit = device type (eg. temp, humidity, ...)
+  value = sensor value
+
+=cut
+
+sub xpl_send_sensor {
+  my ($self, $devid, $unit, $value) = @_;
+
+  my $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'sensor.basic',
+                       body =>
+                       [ 
+                        device => $devid,
+                        type => $unit,
+                        current => $value,
+                       ]);
+  print $xplmsg->summary,"\n" if $self->{_verbose};
+  $self->xpl->send($xplmsg);
+}
+
+=head2 C<xpl_send_x10($device, $command, $protocol, $level)>
+
+This method sends a x10.basic trigger message over the xPL network.
+ device = device identifier (eg. B2, P12...)
+  command = x10 command (on, off)
+  protocol = protocol rf (zwave, x10 ...)
+  level = dim level (0 to 100)
+
+=cut
+
+sub xpl_send_x10 {
+  my ($self, $device, $command, $protocol, $level) = @_;
+  my $xplmsg;
+
+  if ($command eq 'dim' || $command eq 'bright') {
+    $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'x10.basic',
+                       body =>
+                       [
+                        device => lc($device),
+                        command => lc($command),
+                        protocol => lc($protocol),
+			level => $level,
+                       ]);
+  } else {
+    $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'x10.basic',
+                       body =>
+                       [
+                        device => lc($device),
+                        command => lc($command),
+                        protocol => lc($protocol),
+                       ]);
+  }
+  print $xplmsg->summary,"\n" if $self->{_verbose};
+  $self->xpl->send($xplmsg);
+}
+
+=head2 C<xpl_send_rfcmd($device, $command, $protocol, $level)>
+
+This method sends a rfcmd.basic trigger message over the xPL network.
+  device = device identifier (eg. B2, P12...)
+  command = x10 command (on, off)
+  protocol = protocol rf (zwave, x10 ...)
+  level = dim level (0 to 100)
+
+=cut
+
+
+sub xpl_send_rfcmd {
+  my ($self, $device, $command, $protocol, $level) = @_;
+  my $xplmsg;
+
+  if ($command eq 'dim' || $command eq 'bright') {
+    $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'rfcmd.basic',
+                       body =>
+                       [
+                        device => lc($device),
+                        command => lc($command),
+                        protocol => lc($protocol),
+			level => $level,
+                       ]);
+  } else {
+    $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'rfcmd.basic',
+                       body =>
+                       [
+                        device => lc($device),
+                        command => lc($command),
+                        protocol => lc($protocol),
+                       ]);
+  }
+  print $xplmsg->summary,"\n" if $self->{_verbose};
+  $self->xpl->send($xplmsg);
+}
+
+=head2 C<xpl_send_rfcmd($scenario)>
+
+This method sends a x10.basic trigger message over the xPL network.
+  scenario = number of the scenario to execute
+
+=cut
+
+sub xpl_send_scenario {
+  my ($self, $scenario) = @_;
+  my $xplmsg;
+ $xplmsg =
+     xPL::Message->new(message_type => 'xpl-trig',
+                       head => { source => $self->xpl->id, },
+                       schema => 'scenario.basic',
+                       body =>
+                       [
+                        scenario => lc($scenario),
+                        command => "execute",
+                       ]);
+  print $xplmsg->summary,"\n" if $self->{_verbose};
+  $self->xpl->send($xplmsg);
+}
+
+
+sub zibase_rfreceive_decode {
+  my ($self, $msg) = @_;
+
+  if ($msg =~ /^Received radio ID\ /) {
+    # This actually is a RF radio message
+    my $devid = "";
+    if ($msg =~ /\<id\>(\w+)\<\/id\>/) {
+      $devid = $1;
+    }
+    # Try to guess device type for Oregon Scientific sensors
+    if ($msg =~ /THN132N/) {
+      $devid = "thn132n.".$devid;
+    } elsif ($msg =~ /THGR228N/) {
+      $devid = "thgr228n.".$devid;
+    } elsif ($msg =~ /PCR800/) {
+      $devid = "pcr800.".$devid;
+    }
+
+    # Test status value (for Home Automation remotes)
+    if ($msg =~ /\<sta\>\+?(.*)\<\/sta\>/) {
+      my $sta = $1;
+      $self->xpl_send_sensor($devid, 'input', ($sta eq "ON") ? "high" : "low");
+    }
+    # Test temperature value
+    if ($msg =~ /\<tem\>\+?(.*)\<\/tem\>/) {
+      $self->xpl_send_sensor($devid, 'temp', $1);
+    }
+    # Test current rain
+    if ($msg =~ /\<cra\>(.*)\<\/cra\>/) {
+      $self->xpl_send_sensor($devid, 'speed', $1);
+    }
+    # Test Total Rain
+    if ($msg =~ /\<tra\>(.*)\<\/tra\>/) {
+      $self->xpl_send_sensor($devid, 'distance', $1);
+    }
+    # Test Humidity
+    if ($msg =~ /\<hum\>(.*)\<\/hum\>/) {
+      $self->xpl_send_sensor($devid, 'humidity', $1);
+    }
+    # Test UV
+    if ($msg =~ /\<uvl\>(.*)\<\/uvl\>/) {
+      $self->xpl_send_sensor($devid, 'uv', $1);
+    }
+    # Test Power (in kw)
+    if ($msg =~ /\<kw\>(.*)\<\/kw\>/) {
+      $self->xpl_send_sensor($devid, 'power', $1);
+    }
+    # Test energy consumption
+    if ($msg =~ /\<kwh\>(.*)\<\/kwh\>/) {
+      $self->xpl_send_sensor($devid, 'energy', $1);
+    }
+    # Test Wind speed (m/s)
+    if ($msg =~ /\<awi\>(.*)\<\/awi\>/) {
+      $self->xpl_send_sensor($devid, 'speed', $1);
+    }
+    # Test Wind direction
+    if ($msg =~ /\<dir\>(.*)\<\/dir\>/) {
+      $self->xpl_send_sensor($devid, 'direction', $1);
+    }
+	# Test Battery
+	if ($msg =~ /Batt=\<bat\>Ok\<\/bat\>/) {
+	  $self-> xpl_send_sensor($devid, 'battery', 'Ok')
+	}
+	if ($msg =~ /Batt=\<bat\>Low\<\/bat\>/) {
+	  $self-> xpl_send_sensor($devid, 'battery', 'Low')
+	}
+	# Test if this is pure ZWAVE message
+	if ($devid =~ /^([A-P]\d\d?)_ON$/) {
+      $self->xpl_send_rfcmd($1, 'on', 6);
+    }
+    if ($devid =~ /^([A-P]\d\d?)_OFF$/) {
+      $self->xpl_send_rfcmd($1, 'off', 6);
+    }
+	if ($devid =~ /^(Z[A-P]\d\d?)_ON$/) {
+      $self->xpl_send_rfcmd($1, 'on', 6);
+    }
+    if ($devid =~ /^(Z[A-P]\d\d?)_OFF$/) {
+      $self->xpl_send_rfcmd($1, 'off', 6);
+    }
+    # Finally test if this is a pure X10 message
+    if ($devid =~ /^([A-P]\d\d?)$/) {
+      $self->xpl_send_x10($1, 'on', 0);
+    }
+    if ($devid =~ /^([A-P]\d\d?)_OFF$/) {
+      $self->xpl_send_x10($1, 'off', 0);
+    }
+  }
+}
+
+1;
+__END__
+
+=head1 API VERSION
+
+This module has been developed for ZiBases supporting the ZAPI v1.6
+ZiBase firmware should be >= 559
+
+=head1 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+xpl-zibase(1)
+
+Authors website: http://www.poulpy.com
+				Modified by hellorheaven
+
+xPL Perl website: http://www.xpl-perl.org.uk/
+
+Zodianet website: http://www.zodianet.com
+
+=head1 AUTHOR
+
+ZiBase module:
+Thibault Lamy, E<lt>titi@poulpy.comE<gt>
+Mickael Zerbib(alias hellorheaven), E<lt>mickradio@hotmail.comE<gt>
+
+xpl-perl:
+Mark Hindess, E<lt>soft-xpl-perl@temporalanomaly.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011 by Thibault Lamy
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.7 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ lib/xPL/ZiBase/ZiMessage.pm
@@ -0,0 +1,256 @@
+
+
+package ZiMessage;
+
+use Socket;
+
+my $zibase_commands = {
+  'off' => 0,
+  'on'  => 1,
+  'dim' => 2,
+  'bright' => 2,
+  'all_lights_on' => 4,
+  'all_lights_off' => 5,
+  'all_off' => 6,
+  'assoc' => 7,
+  'unassoc' =>8,
+};
+
+my $zibase_protocol = {
+  'preset' => 0,
+  'visonic433' => 1,
+  'visonic868' => 2,
+  'chacon' => 3,
+  'domia' => 4,
+  'x10' => 5,
+  'zwave' => 6,
+  'rfs10' => 7,
+  'xdd433' => 8,
+  'xdd433alrm' => 8,
+  'xdd868' => 9,
+  'xdd868alrm' => 9,
+  'xdd868insh' => 10,
+  'xdd868piwi' => 11,
+  'xdd868boac' => 12,
+};
+
+# new()
+sub new
+{
+  my $class = shift;
+  my $self = {
+    _sig => "ZSIG",
+    _command => 0,
+    _reserved1 => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    _zibase_id => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    _reserved2 => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    _param1 => 0,
+    _param2 => 0,
+    _param3 => 0,
+    _param4 => 0,
+    _my_count => 0,
+    _your_count => 0,
+  };
+
+  bless $self, $class;
+  return $self;
+}
+
+
+=head2 C<getBinaryMessage( )>
+
+This method returns the raw packet string that should be sent to the ZiBase
+
+=cut
+
+sub getBinaryMessage {
+  my ($self) = @_;
+
+  my $data = "ZSIG";
+
+  $data .= pack('n', $self->{_command});
+  $data .= $self->{_reserved1};
+  $data .= $self->{_zibase_id};
+  $data .= $self->{_reserved2};
+  $data .= pack('NNNN', $self->{_param1}, $self->{_param2}, $self->{_param3}, $self->{_param4});
+  $data .= pack('nn', $self->{_my_count}, $self->{_your_count});
+
+  return ($data);
+}
+
+=head2 C<fromPayload($payload)>
+
+This method initializes the ZiMessage object with the given raw packet payload
+Payload generally is a UDP packet received from the zibase.
+
+=cut
+
+sub fromPayload {
+  my ($self, $payload) = @_;
+
+  my @upa = unpack("A4 n A16 A16 A12 N N N N n n A*", $payload);
+	    
+  $self->{_sig} = $upa[0];	    
+  $self->{_command} = $upa[1];
+  $self->{_reserved1} = $upa[2];
+  $self->{_zibase_id} = $upa[3];
+  $self->{_reserved2} = $upa[4];
+  $self->{_param1} = $upa[5];
+  $self->{_param2} = $upa[6];
+  $self->{_param3} = $upa[7];
+  $self->{_param4} = $upa[8];
+  $self->{_my_count} = $upa[9];
+  $self->{_your_count} = $upa[10];
+  $self->{_message} = $upa[11];	    
+}
+
+
+=head2 C<is_ack()>
+
+Returns true if the ZiMessage is a ACK message.
+Returns false otherwise.
+
+=cut
+
+sub is_ack {
+  my ($self) = @_;
+
+  return ($self->{_command} == 14) ? 1 : 0;
+}
+
+=head2 C<is_rfreceive()>
+
+Returns true if the ZiMessage is a RF Receive type command packet.
+Returns false otherwise.
+
+=cut
+
+sub is_rfreceive {
+  my ($self) = @_;
+
+  return ($self->{_command} == 3) ? 1 : 0;
+}
+
+=head2 C<setRegisterHost($ip, $port)>
+
+Sets the ZiMessage to be a Register Host zibase message
+
+=cut
+
+sub setRegisterHost {
+  my ($self, $ip, $port) = @_;
+
+  $self->{_command} = 13;
+  $self->{_param1} = unpack("N*", inet_aton($ip));
+  $self->{_param2} = $port;
+  $self->{_param3} = 0;
+  $self->{_param4} = 0;
+  #$self->{_reserved1} = "ZapiInit\x00\x00\x00\x00\x00\x00\x00\x00";
+}
+
+=head2 C<setunRegisterHost($ip, $port)>
+
+Sets the ZiMessage to  Unregister the Host zibase from the Zibase (not used for the moment)
+
+=cut
+
+sub setUnRegisterHost {
+  my ($self, $ip, $port) = @_;
+
+  $self->{_command} = 22;
+  $self->{_param1} = unpack("N*", inet_aton($ip));
+  $self->{_param2} = $port;
+  $self->{_param3} = 0;
+  $self->{_param4} = 0;
+}
+
+=head2 C<setRFCommand($command, $protocol, $level, $nbrepeat)>
+
+Sets the ZiMessage to be a RF command send message with the given
+parameters :
+  command should be one of [on|off|dim]
+  level should be a number between 0 and 100 (only used if command=dim)
+  nbrepeat sets the desired packet repetitions
+
+=cut
+
+sub setRFCommand {
+  my ($self, $command, $protocol, $level, $nbrepeat) = @_;
+
+  # Sets global command type
+  $self->{_command} = 11;
+  $self->{_param1} = 0;
+
+  # Sets the HA command code
+  my $prm = $zibase_commands->{lc($command)};
+  
+  # Sets the Protocol code
+  my $proto = $zibase_protocol->{lc($protocol)};
+  $prm = $prm | ($proto & 0xFF) <<0x08;
+
+  # Sets the dim level if needed
+  if ($prm == $zibase_commands->{'dim'}) {
+    $prm = $prm | ($level & 0xFF) << 0x16;
+  }
+
+  # Sets the burst if specified
+  if (defined($nbrepeat) && $nbrepeat > 1) {
+    $prm = $prm | ($nbrepeat & 0xFF) << 0x24;
+  }
+
+  $self->{_param2} = $prm;
+}
+
+=head2 C<setRFexecScenario($scenario)>
+
+Sets the ZiMessage to be a RF command send message with the given
+parameters :
+  scenario should be the number of scenario you would be launch 
+
+=cut
+
+sub setRFexecScenario {
+  my ($self, $scenario) = @_;
+
+  # Sets global command type
+  $self->{_command} = 11;
+  $self->{_param1} = 1;
+  $self->{_param2} = $scenario;
+}
+
+
+
+=head2 C<setRFAddress($address)>
+
+Sets the ZiMessage x10 address. Only applicable to RF Send messages.
+parameters :
+  address is a string representing the x10-like address (a5, p10...)
+
+=cut
+
+sub setRFAddress {
+  my ($self, $device) = @_;
+
+  my ($nb1, $nb2) = $self->decode_x10_address($device);
+  $self->{_param3} = $nb2;
+  $self->{_param4} = $nb1;
+}
+
+=head2 C<decode_x10_address($address)>
+
+Decodes the given x10 address in string format to integers.
+Returns an array of two integers representing the house and unit codes.
+
+=cut
+
+sub decode_x10_address {
+  my ($self, $address) = @_;
+
+  #$device = lc($device);
+  my $nb1 = ord($address) - ord('a');
+  my $nb2 = int(substr($address, 1)) - 1;
+  return ($nb1, $nb2);
+}
+
+__END__
+
