351128f7e731a5c8406b84c39e5f51b5d0284ac8
Mickael ZERBIB <mickyradio@hotmail.com>
2012-11-05 14:11:32 +0100

    adding launching script

--- lib/xPL/ZiBase/ZiMessage.pm
+++ lib/xPL/ZiBase/ZiMessage.pm
@@ -78,7 +78,7 @@ sub getBinaryMessage {
   $data .= $self->{_reserved2};
   $data .= pack('NNNN', $self->{_param1}, $self->{_param2}, $self->{_param3}, $self->{_param4});
   $data .= pack('nn', $self->{_my_count}, $self->{_your_count});
-  $data .= $self->{_message};
+
 
   return ($data);
 }
